976a0439299f01f1e6bc7d20bd1c80cb5912fad3
satrana24 <sofiaselkova163@gmail.com>
2023-02-28 17:19:18 +0300

    homework1

--- дз3.py
+++ дз3.py
@@ -1,6 +1,6 @@
 year = int(input())
 
-if (year % 4 == 0 or year % 100 != 0) or year % 100 != 0:
+if year % 4 == 0:
     print(f'{year} год високосный')
 else:
     print(f'{year} год не високосный')
